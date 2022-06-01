e7ddf56722bf243109202202fb676adf2226cf4f
francisco-vr <fvillarroel21@gmail.com>
2022-06-01 00:21:50 -0400

    update beamer and script

--- Scripts/Exp2_Boston.R
+++ Scripts/Exp2_Boston.R
@@ -10,10 +10,11 @@ ipak <- function(pkg){
 }
 
 # usage
+
 packages <- c("tidyverse","dplyr","haven","ggplot2","readxl","summarytools", "patchwork","stringr",
               "tidyr","kableExtra","psych", "MASS", "foreign", "data.table","gtools","lubridate","AER",
               "xtable","pBrackets","Hmisc","ri","ggpubr", "stargazer", "Rmisc","wesanderson", "gridExtra","ggmosaic",
-              "vcd", "plyr", "ggannotate","scales", "fastDummies")
+              "vcd", "plyr", "ggannotate","scales", "fastDummies","gt")
 ipak(packages)
 
 # READ DF
@@ -40,24 +41,47 @@ df$SC0 <-as.numeric(df$SC0)
 Model1 <-glm(SC0 ~ E2Treat_Afin + E2Treat_Opuesto, data = df)
 Model2 <-glm(SC0 ~ E2Treat_Afin + E2Treat_Opuesto + HomoIndex, data = df)
 Model3 <-glm(SC0 ~ E2Treat_Afin + E2Treat_Opuesto + HomoIndex + DigitIndex, data = df)
-Model4 <-glm(SC0 ~ E2Treat_Afin + E2Treat_Opuesto + HomoIndex + DigitIndex + Age_2 + Age_3 + Age_4,
-              data = df)
-Model5 <-glm(SC0 ~ E2Treat_Afin + E2Treat_Opuesto + HomoIndex + DigitIndex + Age_2 + Age_3 + Age_4 + Educ_2
+Model4 <-glm(SC0 ~ E2Treat_Afin + E2Treat_Opuesto + HomoIndex + DigitIndex + SexDum, data = df)
+Model5 <-glm(SC0 ~ E2Treat_Afin + E2Treat_Opuesto + HomoIndex + DigitIndex + SexDum + Age_2 + Age_3 + Age_4, data = df)
+Model6 <-glm(SC0 ~ E2Treat_Afin + E2Treat_Opuesto + HomoIndex + DigitIndex + SexDum + Age_2 + Age_3 + Age_4 + Educ_2
              + Educ_3 + Educ_4 + Educ_5, data = df)
-Model6 <-glm(SC0 ~ E2Treat_Afin + E2Treat_Opuesto + HomoIndex + DigitIndex + Age_2 + Age_3 + Age_4 + Educ_2
+Model7 <-glm(SC0 ~ E2Treat_Afin + E2Treat_Opuesto + HomoIndex + DigitIndex + SexDum + Age_2 + Age_3 + Age_4 + Educ_2
              + Educ_3 + Educ_4 + Educ_5 + ideologia_Izquierda + ideologia_Derecha + ideologia_Ninguno,
              data = df)
-Model7 <-glm(SC0 ~ E2Treat_Afin + E2Treat_Opuesto + HomoIndex + DigitIndex + Age_2 + Age_3 + Age_4 + Educ_2
+Model8 <-glm(SC0 ~ E2Treat_Afin + E2Treat_Opuesto + HomoIndex + DigitIndex + SexDum + Age_2 + Age_3 + Age_4 + Educ_2
              + Educ_3 + Educ_4 + Educ_5 + ideologia_Izquierda + ideologia_Derecha + ideologia_Ninguno
-             + NivEco_2 + NivEco_3 + NivEco_4 + NivEco_5,
-             data = df)
+             + NivEco_2 + NivEco_3 + NivEco_4 + NivEco_5, data = df)
 
-Reg1Bal <-modelsummary::msummary(list(Model1, Model2, Model3), statistic = 'p.value', starts = TRUE)
+cm <-c('(Intercept)' = 'Constant', 'E2Treat_Afin' = 'T1 Like-Minded', 'E2Treat_Opuesto' = 'T2 Opposite',
+       'HomoIndex' = 'High Eco Chamber', 'DigitIndex' = 'High Digital Citizenship')
+
+cap <-'regression table with balanced variables'
+
+
+Reg1Bal <-modelsummary::msummary(list(Model1, Model2, Model3),
+                                 output = "gt", coef_map = cm, title = cap,
+                                 starts = TRUE, statistic = 'p.value')
+Reh1Bal <-Reg1Bal%>%
+  tab_footnote(footnote = md("A very important variable."))
+
 Reg1Bal
-# Regresion with unbalanced covariates
 
+## Not balanced regression
 
-Reg1NoBa <-modelsummary::msummary(list(Model1, Model2, Model3, Model4, Model5, Model6,Model7))
-Reg1NoBa
+ct <-c('(Intercept)' = 'Constant', 'E2Treat_Afin' = 'T1 Like-Minded', 'E2Treat_Opuesto' = 'T2 Opposite',
+       'HomoIndex' = 'High Eco Chamber', 'DigitIndex' = 'High Digital Citizenship', 'SexDum' = 'Women', 'Age_2' = '30 to 40 years',
+       'Age_3' = '41 to 65 years', 'Age_4' = '66+ years', 'Educ_2' = 'Elemental School', 'Educ_3' = 'Secondary School',
+       'Educ_4' = 'Graduate', 'Educ_5' = 'Postgraduate', 'ideologia_Izquierda' = 'left-wing', 'ideologia_Derecha' = 'right-wing',
+       'ideologia_Ninguno' = 'Without Ideology', 'NivEco_2' = '$224.001 - $448-000', 'NivEco_3' = '$448.001 - $1.000.000',
+       'NivEco_4' = '$1.000.001 - $3.000.000', 'NivEco_5' = '$3.000,000+')
 
+titulo <-'Regression with not-balanced variables'
+
+Reg1NoBal <-modelsummary::msummary(list(Model1, Model2, Model3, Model4, Model5, Model6, Model7, Model8),
+                                   output = "gt", title = titulo, coef_map = 'ct', Starts = TRUE)
+
+Reg1NoBal
+library(xaringanBuilder)
+
+build_pdf("beamer_presentation/beamer_presentation.Rmd")
 
