c0d911c2476c746fbda43bc6fed78400f19801b2
francisco-vr <fvillarroel21@gmail.com>
2022-05-31 21:55:46 -0400

    update images and beamer

--- Scripts/Exp2_Boston.R
+++ Scripts/Exp2_Boston.R
@@ -10,7 +10,10 @@ ipak <- function(pkg){
 }
 
 # usage
-packages <- c("tidyverse","dplyr", "patchwork","kableExtra","MASS","ggpubr","fastDummies")
+packages <- c("tidyverse","dplyr","haven","ggplot2","readxl","summarytools", "patchwork","stringr",
+              "tidyr","kableExtra","psych", "MASS", "foreign", "data.table","gtools","lubridate","AER",
+              "xtable","pBrackets","Hmisc","ri","ggpubr", "stargazer", "Rmisc","wesanderson", "gridExtra","ggmosaic",
+              "vcd", "plyr", "ggannotate","scales", "fastDummies")
 ipak(packages)
 
 # READ DF
@@ -26,7 +29,8 @@ df <-fastDummies::dummy_cols(df, select_columns = 'Age')
 df <-fastDummies::dummy_cols(df, select_columns = 'Educ')
 df <-fastDummies::dummy_cols(df, select_columns = 'E2Treat')
 df <-fastDummies::dummy_cols(df, select_columns = 'ideologia')
-df <-dplyr::select(df, -Age_1, -Educ_1, -E2Treat_Control, -ideologia_centro)
+df <-fastDummies::dummy_cols(df, select_columns = 'NivEco')
+df <-dplyr::select(df, -Age_1, -Educ_1, -E2Treat_Control, -ideologia_centro, -NivEco_1)
 
 
 # Regresion with balanced covariates
@@ -43,14 +47,17 @@ Model5 <-glm(SC0 ~ E2Treat_Afin + E2Treat_Opuesto + HomoIndex + DigitIndex + Age
 Model6 <-glm(SC0 ~ E2Treat_Afin + E2Treat_Opuesto + HomoIndex + DigitIndex + Age_2 + Age_3 + Age_4 + Educ_2
              + Educ_3 + Educ_4 + Educ_5 + ideologia_Izquierda + ideologia_Derecha + ideologia_Ninguno,
              data = df)
+Model7 <-glm(SC0 ~ E2Treat_Afin + E2Treat_Opuesto + HomoIndex + DigitIndex + Age_2 + Age_3 + Age_4 + Educ_2
+             + Educ_3 + Educ_4 + Educ_5 + ideologia_Izquierda + ideologia_Derecha + ideologia_Ninguno
+             + NivEco_2 + NivEco_3 + NivEco_4 + NivEco_5,
+             data = df)
 
-Reg1Bal <-modelsummary::msummary(list(Model1, Model2, Model3), stars = TRUE)
+Reg1Bal <-modelsummary::msummary(list(Model1, Model2, Model3), statistic = 'p.value', starts = TRUE)
 Reg1Bal
-
-
 # Regresion with unbalanced covariates
 
 
-Reg1NoBa <-modelsummary::msummary(list(Model1, Model2, Model3, Model4, Model5, Model6), statistic = 'p.value', stars = TRUE)
+Reg1NoBa <-modelsummary::msummary(list(Model1, Model2, Model3, Model4, Model5, Model6,Model7))
 Reg1NoBa
+
 
